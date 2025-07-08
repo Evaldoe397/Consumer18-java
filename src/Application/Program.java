e81850ee4983db8330aa0dacd5aee065e296d40f
evaldoe397 <evaldoe397@gmail.com>
2025-07-08 18:34:36 -0300

    Expressao Lambda inline

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -15,18 +15,12 @@ public class Program {
 		
 		List<Product> list = new ArrayList<>();
 		
-		double factor = 1.1;
-		
-		Consumer<Product> cons = p -> {
-			p.setPrice(p.getPrice() * factor);
-			};
-		
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(cons);
+		list.forEach(p -> p.setPrice(p.getPrice()*1.1));
 		
 		list.forEach(System.out::println);
 	}
