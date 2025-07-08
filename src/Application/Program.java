d1900b4c25377b14ee201b0e1dcd5f3808882fea
evaldoe397 <evaldoe397@gmail.com>
2025-07-08 18:28:00 -0300

    Expressao Lambda declarada

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -2,6 +2,8 @@ package Application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+
 import util.PriceUpdate;
 
 import entities.Product;
@@ -13,12 +15,18 @@ public class Program {
 		
 		List<Product> list = new ArrayList<>();
 		
+		double factor = 1.1;
+		
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+			};
+		
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 	}
